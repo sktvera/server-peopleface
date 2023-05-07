eeebd0fe40e1658930f5f7b891698867c89dcd14
sktvera <veeraa1270@gmail.com>
2023-05-07 14:11:11 -0500

    jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {User} from "./user.entity";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'mysql-peopleface.alwaysdata.net',
+            port: 3306,
+            username: '312402_jdvera',
+            password: 'jdvg12700',
+            database: 'peopleface_dbpf',
+            entities: [User],
+            synchronize: true,
+        }),
+        TypeOrmModule.forFeature([User]),
+        JwtModule.register({
+            secret: 'secret',
+            signOptions: {expiresIn: '1d'}
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+export class AppModule {
+}
